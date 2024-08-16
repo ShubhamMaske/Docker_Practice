9e03cae623ebd8eb4893ed72ffa730729a0a5101
Shubham Maske <shubhammaske099@gmail.com>
2024-08-16 13:54:16 +0530

    basic server set-up

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: "shubh",
+            age: 24
+        },
+        {
+            id: 2,
+            name: "raj",
+            age: 23
+        },
+        {
+            id: 1,
+            name: "Shivam",
+            age: 25
+        }
+    ])
+})
+
+app.listen(4000, () => {
+    console.log("server is running on port 4000")
+})
